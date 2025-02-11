0a3faf9450d0c8c3531d3dd505078fcba985c093
TaleemHossain <137270943+TaleemHossain@users.noreply.github.com>
2025-02-12 03:03:28 +0530

    fix: up to movement

---
+++ Assets/Scripts/Player Controller.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed = 1f;
+
+    private PlayerControl playerControl;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRenderer;
+    private void Awake()
+    {
+        playerControl = new PlayerControl();
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void OnEnable()
+    {
+        playerControl.Enable();
+    }
+    private void Update()
+    {
+        PlayerInput();
+    }
+    private void FixedUpdate()
+    {
+        AdjustPlayerFacingDirection();
+        Move();
+    }
+    private void PlayerInput() 
+    {
+        movement = playerControl.Movement.Move.ReadValue<Vector2>();
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
+    }
+    private void Move()
+    {
+        rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+    private void AdjustPlayerFacingDirection() 
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        if(mousePos.x < playerScreenPoint.x) {
+            mySpriteRenderer.flipX = false;
+        } else {
+            mySpriteRenderer.flipX = true;
+        }
+    }
+}
