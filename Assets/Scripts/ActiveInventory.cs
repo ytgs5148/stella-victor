a85a9fde10da81f12d06f7452fbfed3f7ee27def
TaleemHossain <137270943+TaleemHossain@users.noreply.github.com>
2025-02-14 18:26:25 +0530

    feat: weapons as scriptable objects

--- Assets/Scripts/ActiveInventory.cs
+++ Assets/Scripts/ActiveInventory.cs
@@ -29,5 +29,9 @@ public class ActiveInventory : MonoBehaviour
             inventorySlot.GetChild(0).gameObject.SetActive(false);
         }
         this.transform.GetChild(indexNum).GetChild(0).gameObject.SetActive(true);
+        ChangeActiveWeapon();
+    }
+    private void ChangeActiveWeapon() {
+        Debug.Log(transform.GetChild(activeSlotIndexNum).GetComponent<InventorySlot>().GetWeaponInfo().weaponPrefab.name);
     }
 }
--- Assets/Scripts/InventorySlot.cs
+++ Assets/Scripts/InventorySlot.cs
@@ -2,15 +2,8 @@ using UnityEngine;
 
 public class InventorySlot : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    [SerializeField] private WeaponInfo weaponInfo;
+    public WeaponInfo GetWeaponInfo() {
+        return weaponInfo;
     }
 }
