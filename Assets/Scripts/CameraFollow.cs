b9d529b1917dcae205ee7667976b17309b06ef95
Rudranil Basak <72183312+ytgs5148@users.noreply.github.com>
2025-02-14 16:01:01 +0530

    fix: player game object

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float lerpSpeed = 1.0f;
+
+    private Vector3 offset;
+
+    private Vector3 targetPos;
+
+    private void Start()
+    {
+        if (PlayerController.Instance == null) return;
+
+        offset = transform.position - PlayerController.Instance.transform.position;
+    }
+
+    private void Update()
+    {
+        if (PlayerController.Instance == null) return;
+
+        targetPos = PlayerController.Instance.transform.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetPos, lerpSpeed * Time.deltaTime);
+    }
+
+}
