a440fa5285ce0e9256958915df8cbdc251b44420
TaleemHossain <137270943+TaleemHossain@users.noreply.github.com>
2025-02-15 17:29:59 +0530

    fix: in bow and arrow

--- Assets/Scripts/Rifle.cs
+++ Assets/Scripts/Rifle.cs
@@ -6,20 +6,22 @@ public class Rifle : MonoBehaviour, IWeapon
     {
         MouseFollowWithOffset();
     }
-    public void Attack() {
+    public void Attack()
+    {
         Debug.Log("Rifle Attack");
         StartCoroutine(ResetAttack());
     }
 
-    private IEnumerator ResetAttack() {
+    private IEnumerator ResetAttack()
+    {
         yield return new WaitForSeconds(0.75f);
         ActiveWeapon.Instance.ToggleIsAttacking(false);
     }
-    private void MouseFollowWithOffset() {
+    private void MouseFollowWithOffset()
+    {
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3 gunPos = ActiveWeapon.Instance.transform.position;
 
-        // Get the direction from the gun to the mouse
         Vector3 direction = mousePos - gunPos;
 
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
@@ -27,7 +29,6 @@ public class Rifle : MonoBehaviour, IWeapon
         bool isLeft = mousePos.x < gunPos.x;
         ActiveWeapon.Instance.transform.localScale = new Vector3(1, isLeft ? -1 : 1, 1);
 
-        // Smoothly rotate the gun
         float smoothedAngle = Mathf.LerpAngle(ActiveWeapon.Instance.transform.eulerAngles.z, angle, Time.deltaTime * 10);
         ActiveWeapon.Instance.transform.rotation = Quaternion.Euler(0, 180, -smoothedAngle);
     }
